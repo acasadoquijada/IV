2b898f07ff7caa7d2ca3528ecc16e35eab1f087e
alejandro <acasadoquijada@gmail.com>
2016-01-21 19:59:55 +0100

    Despliegue

--- proyectoDAI/settings.py
+++ proyectoDAI/settings.py
@@ -144,8 +144,6 @@ STATICFILES_DIRS = (
     STATIC_PATH,
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 REGISTRATION_OPEN = True                # If True, users can register
 ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
--- proyectoDAI/wsgi.py
+++ proyectoDAI/wsgi.py
@@ -9,11 +9,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyectoDAI.settings")
-
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyectoDAI.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
