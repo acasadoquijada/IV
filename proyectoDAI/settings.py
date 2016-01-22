f56015b7d7baa84fbe18cf0554a652d997704706
alejandro <acasadoquijada@gmail.com>
2016-01-22 10:20:29 +0100

    Puesta en producción closed #13 espero

--- proyectoDAI/settings.py
+++ proyectoDAI/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-vsi*yr21o3)d%-u%ho28+tdci&afj5-lk4sqo#c%-9(itd!v@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
